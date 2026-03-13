62b2f129eeb2f77839697e31e3f0b38810cbf2ef
Ahmed Gamal <ahmed.gamal9615@gmail.com>
2026-03-13 03:15:44 +0200

    Answer for quest (8 , 9 , 10 , 11 , 13 , 14 , 15)

--- assignment1.js
+++ assignment1.js
@@ -70,5 +70,71 @@
 // });
 // console.log(getLength);
 
+// 8. Write a function that checks if a number is divisible by 3 and 5.
+// function check(num) {
+//     if (num % 3 === 0 && num % 5 === 0) {
+//         console.log("The number divisible by both ( 5 , 3 )"); 
+//         return num ;
+//     }else if (num % 3 === 0) {
+//         console.log("The number divisible by 3 only");
+//         return num ;
+//     }else if (num % 5 === 0) {
+//         console.log("The number divisible by 5 only");
+//         return num ;
+//     } else {
+//         console.log("The number not divisible by both ( 5 , 3 )");
+//         return false ;
+//     }   
+// };
+// console.log(check(15));
+
+// 9. Write a function using arrow syntax to return the square of a number .
+// const getSquareOfNum = (number) => {
+//     console.log(`The Square of ${number} is :` , number * number);
+//     let x = number * number ;
+//     return (x) ;
+// } ;
+// console.log(getSquareOfNum (10)) ;
+
+// 10.Write a function that destructures an object to extract values and returns a formatted string.
+// function destPersonInfo(person){
+//    const { name , age } = person;
+//    return `${name} is ${age} years old` ;
+// }
+// const person = { name : `john` , age : `25`} ;
+// console.log(destPersonInfo(person));
+
+// 11.Write a function that accepts multiple parameters (two or more) and returns their sum.
+// function multiple (num1 , num2 , ...numbers){
+// const sumNum = [num1 , num2 , ...numbers] ;
+// return sumNum.reduce((num1 , num2)=> num1 + num2 , 0) ;
+// }
+// console.log(multiple(1 , 2 , 3 , 4 , 5 , 6 , 7));
+
+// 13. Write a function to find the largest number in an array.
+// function getMaxNum (num1 , num2 , ...numbers){
+//     const allNum = [num1 , num2 , ...numbers] ;
+//     let x = Math.max(...allNum) ;
+//     return x ;
+// }
+// let result = getMaxNum(1 , 2 , 5 , 7 , 3 , 11 , 15 , 20);
+// console.log("The Result Is" , result);
+
+//14. Write a function that takes an object and returns an array containing only its keys.
+// function getKeysOfObject(object) {
+//     return Object.keys(object) ;
+// }
+// const output = getKeysOfObject(object = {name : "Ahmed" , age : 25 , gender : "Male"} )
+// console.log(output);
+
+// 15. Write a function that splits a string into an array of words based on spaces.\
+// function splitOfString (string){
+//  return string.split(' ') ;
+// }
+// let output = splitOfString("Ahmed Is Good BackEnd Developer")
+// console.log(output);
+
+
+
 
 
